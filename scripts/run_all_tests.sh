54427eb33d91f9ea125c3944f723fdc9fa538a11
Isaac Holt <isaac_holt@icloud.com>
2023-01-02 17:03:43 +0000

    Make num crates default features set to false, update test scripts

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -7,7 +7,7 @@ test_integer_info () {
 
 run_test () {
 	test_integer_info "$1"
-	RUSTFLAGS="--cfg test_int_bits=\"$1\"" cargo test int --lib --quiet --features "rand numtraits nightly serde"
+	RUSTFLAGS="--cfg test_int_bits=\"$1\"" cargo test int --lib --quiet --all-features
 	if [ $? -ne 0 ]
 	then
 		exit 1
