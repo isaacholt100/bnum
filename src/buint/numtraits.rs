5c1c879692487cf7654d10d66f102f57c5cd6408
Isaac Holt <isaac_holt@icloud.com>
2023-07-23 14:16:58 +0100

    Fix error

--- src/buint/numtraits.rs
+++ src/buint/numtraits.rs
@@ -49,28 +49,7 @@ pub const fn u32_bits(u: u32) -> ExpType {
 pub const fn u64_bits(u: u64) -> ExpType {
     64 - u.leading_zeros() as ExpType
 }
-
-macro_rules! decode_float {
-    ($name: ident, $f: ty, $u: ty) => {
-        pub fn $name(f: $f) -> ($u, i16) {
-            const BITS: u32 = core::mem::size_of::<$f>() as u32 * 8;
-            const MANT_MASK: $u = <$u>::MAX >> (BITS - (<$f>::MANTISSA_DIGITS - 1));
-            const EXP_MASK: $u = <$u>::MAX >> 1;
-            const BIAS: i16 = <$f>::MAX_EXP as i16 - 1;
-
-            let bits = f.to_bits();
-            let exp = ((bits & EXP_MASK) >> (<$f>::MANTISSA_DIGITS - 1)) as i16;
-            let mut mant = bits & MANT_MASK;
-            if exp != 0 {
-                mant |= (1 << (<$f>::MANTISSA_DIGITS - 1));
-            }
-            (mant, exp - (BIAS + <$f>::MANTISSA_DIGITS as i16 - 1))
-        }
-    };
-}
-
-decode_float!(decode_f32, f32, u32);
-decode_float!(decode_f64, f64, u64);
+use crate::buint::cast::{decode_f32, decode_f64};
 //use crate::nightly::impl_const;
 use crate::ExpType;
 use num_integer::{Integer, Roots};
